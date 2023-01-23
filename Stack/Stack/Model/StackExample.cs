e162a1f81d73b427cc39005d9788e69b5eff18e7
nuna092 <56553943+nuna092@users.noreply.github.com>
2023-01-23 15:37:24 +0300

    First commit
    Добавил пример реализации стека

---
+++ Stack/Stack/Model/StackExample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack.Model
+{
+    public class StackExample<T>
+    {
+
+        private List<T> _items = new List<T>();
+        public int Count => _items.Count;
+
+        public void Push(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            _items.Add(item);
+        }
+
+        public T Peek()
+        {
+            var item = _items.LastOrDefault();
+
+            if (item == null)
+                ThorwNullReferenceException("Cтек пустой. Нет элементов.");
+
+            return item;
+        }
+
+        public T Pop()
+        {
+            var item = _items.LastOrDefault();
+
+            if (item == null)
+                ThorwNullReferenceException("Cтек пустой. Нет элементов для получения.");
+
+            _items.RemoveAt(_items.Count - 1);
+
+            return item;
+        }
+        private void ThorwNullReferenceException(string message)
+        {
+            throw new NullReferenceException(message);
+        }
+
+
+    }
+    }
+
+
---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,34 @@
+﻿using Stack.Model;
+using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            var stack = new StackExample<int>();
+
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(34);
+            stack.Push(-1);
+            stack.Push(0);
+            Console.Write($"Стек содержит {stack.Count} элементов.");
+
+            var item1 = stack.Pop();
+            Console.WriteLine($"Верхний элемент {item1}.");
+            var item2 = stack.Pop();
+            Console.WriteLine($"Предпоследний элемент {item2}.");
+
+            stack.Push(88);
+            var item3 = stack.Peek();
+            Console.WriteLine($"Новый верхний элемент {item3}.");
+            var item4 = stack.Peek();
+            Console.WriteLine($"Новый верхний элемент {item4}.");
+
+            Console.ReadLine();
+        }
+    }
+}
+
